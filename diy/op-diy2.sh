a29491a20ddf20b543d6302e81cb8d969aa16b41
cddcx <84568016+cddcx@users.noreply.github.com>
2026-04-27 22:12:11 +0800

    Update op-diy2.sh

--- diy/op-diy2.sh
+++ diy/op-diy2.sh
@@ -78,8 +78,8 @@ sed -i "s/DEFAULT_PACKAGES.router:=/DEFAULT_PACKAGES.router:=default-settings-ch
 sed -i 's/DEFAULT_PACKAGES += /DEFAULT_PACKAGES += luci-app-daed /g' target/linux/rockchip/Makefile
 
 # 更新 golang 版本
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
+#rm -rf feeds/packages/lang/golang
+#git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
 
 # nanopc-t6
 echo '# 启用 eBPF 支持
