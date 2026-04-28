c9292e4b73857aa3349aa4cc9cdebaea6f78e68d
cddcx <84568016+cddcx@users.noreply.github.com>
2026-04-28 08:26:35 +0800

    Update im-diy2.sh

--- diy/im-diy2.sh
+++ diy/im-diy2.sh
@@ -77,7 +77,7 @@ rm -rf feeds/packages/net/{adguardhome,alist,daed,v2raya,microsocks,shadowsocks-
 #sed -i 's/dnsmasq/dnsmasq-full/g' include/target.mk
 sed -i "s/kmod-nft-offload/kmod-nft-offload kmod-nft-tproxy/" include/target.mk
 #sed -i "s/odhcp6c/ipv6-helper/" include/target.mk
-sed -i "s/DEFAULT_PACKAGES.router:=/DEFAULT_PACKAGES.router:=default-settings-chn vmlinux-btf luci-app-firewall /" include/target.mk
+sed -i "s/DEFAULT_PACKAGES.router:=/DEFAULT_PACKAGES.router:=default-settings-chn luci-app-firewall /" include/target.mk
 
 # 删除luci-app-cpufreq
 #sed -i '#luci-app-cpufreq \#d' include/target.mk
