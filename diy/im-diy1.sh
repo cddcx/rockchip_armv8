83b240dad11fa6283b6995a572ef7ca14d70ef6a
cddcx <84568016+cddcx@users.noreply.github.com>
2026-06-23 11:28:36 +0800

    Update im-diy1.sh

--- diy/im-diy1.sh
+++ diy/im-diy1.sh
@@ -59,6 +59,7 @@ git clone https://github.com/sbwml/luci-app-quickfile package/OpenWrt-quickfile
 # luci-app-daed
 git clone https://github.com/QiuSimons/vmlinux-btf package/vmlinux-btf
 git clone https://github.com/QiuSimons/luci-app-daed package/dae
+merge_package v5 https://github.com/sbwml/openwrt_helloworld package/dae v2ray-geodata
 #merge_package v5 https://github.com/sbwml/openwrt_helloworld package/dae daed vmlinux-btf v2ray-geodata luci-app-daed
 
 # 添加rtp2httpd
